a8d521b061b3f688c14e973044be084171717f1c
Entropie67 <olivier.elophe@gmail.com>
2022-03-24 12:02:52 +0100

    créationd de la classe match

--- modeles/match.py
+++ modeles/match.py
@@ -1,12 +1,30 @@
+from random import randint
 class Match:
     """
         Classe qui modélise un match
     """
 
     def __init__(self, joueur_a, joueur_b):
-        self.player1 = joueur_a
-        self.player2 = joueur_b
-        self.resultat = ""
+        self.player_blanc = joueur_a
+        self.player_noir = joueur_b
+        self._resultat = self._deroulement()
 
     def __str__(self):
-        return f" \n {self.player1} vs. {self.player2} => Résultat {self.resultat}"
+        return f" \n {self.player_blanc} avec les blanc vs. {self.player_noir} avec les noirs" \
+               f" => Résultat: {self.resultat}"
+
+    def _deroulement(self):
+        """
+            déroulent automatique du match avec pondération en fonctione l'elo
+            0 pour un match nul, 1 pour une victoire des blancs et 2 pour une victoire des noirs
+        """
+        # Pondération à ajouter
+        return randint(0, 2)
+
+    @property
+    def resultat(self):
+        return self._resultat
+
+    @resultat.setter
+    def resultat(self, val):
+        self._resultat = val
---
+++ modeles/ronde.py
@@ -0,0 +1,7 @@
+class Ronde:
+    """
+        Classe modélisant une ronde (un ensemble de match)
+    """
+
+    def __init__(self, liste_match):
+        self.liste_matche = liste_match
