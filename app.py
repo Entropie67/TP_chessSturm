94f214753539b888ab7d838e1985e27d27ad0c88
Entropie67 <olivier.elophe@gmail.com>
2022-03-24 11:03:22 +0100

    Début de player

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from modeles.joueur import Joueur
+
+
 
 def main():
-
+    player1 = Joueur("Kasparov", "Gary", )
 
 
 if __name__ == "__main__":
--- modeles/joueur.py
+++ modeles/joueur.py
@@ -1,3 +1,13 @@
 
 
 class Joueur:
+    """
+        class joueur
+    """
+
+    def __init__(self, name, age, sexe, date, elo):
+            self.name = name
+            self.age = age
+            self.sexe = sexe
+            self.date_naissance = date
+            self.classement = elo
