0ad4d630281b9aef3b562fe71459ce2bfd0477e9
Olivier <olivier.elophe@gmail.com>
2022-05-11 18:54:23 +0200

    typing pour la classe joueur

--- modeles/joueur.py
+++ modeles/joueur.py
@@ -27,20 +27,20 @@ class Joueur:
     def classement(self, val: int) -> None:
         self.elo = val
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: 'Joueur') -> bool:
         """ Méthode spéciale pour tester l'égalité de classement de deux joueurs"""
         if self.elo == other.classement:
             return True
         else:
             return False
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: 'Joueur') -> bool:
         if self.elo < other.classement:
             return True
         else:
             return False
 
-    def __gt__(self, other) -> bool:
+    def __gt__(self, other: 'Joueur') -> bool:
         if self.elo > other.classement:
             return True
         else:
